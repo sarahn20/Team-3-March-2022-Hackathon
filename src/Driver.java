1cd984e9e8b2073718f0051f9035ceafd0120014
jonnyhappyfeet <jonathan.yakimow@cuw.edu>
2022-03-05 15:47:30 -0600

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.ActionMap;
@@ -12,32 +13,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException; 
-import javax.swing.*;
-//import sun.audio.*;
-import java.io.*;
-import java.awt.*;
-import javax.sound.*;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Driver {
 
-	//private static AudioStream theStream;
-	
     public static void main(String[] args) {
         new Driver();
-        /*try
-        {
-            theStream = new AudioStream(new FileInputStream("src//tetris.wav"));
-            AudioPlayer.player.start(theStream);
-            System.out.println("reached");
-        }
-        catch(Exception e)
-        {
-        	e.printStackTrace();
-        	System.out.println("reached (exception)");
-        } */
     }
-    
+
     public Driver() {
         EventQueue.invokeLater(new Runnable() {
             @Override
@@ -62,7 +45,51 @@ public class Driver {
 
         private int xPos;
         private int yPos;
+        private int x2Pos = 100;
+        private int y2Pos= 100;
+        Random rand = new Random();
         public TestPane() {
+            Action left2Action = new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                	x2Pos -= 2;
+                    if (x2Pos < 0) {
+                    	x2Pos = 0;
+                    }
+                    repaint();
+                }
+            };
+            Action right2Action = new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                	x2Pos += 2;
+                    if (x2Pos + 10 > getWidth()) {
+                    	x2Pos = getWidth() - 10;
+                    }
+                    repaint();
+                }
+            };
+            Action down2Action = new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                	y2Pos += 2;
+                    if (y2Pos + 10 > getWidth()) {
+                    	y2Pos = getWidth() - 10;
+                    }
+                    repaint();
+                }
+            };
+            Action up2Action = new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                	y2Pos -= 2;
+                    if (y2Pos < 0) {
+                    	y2Pos = 0;
+                    }
+                    repaint();
+                }
+            };
+        	/////////////////////////////////////
             Action leftAction = new AbstractAction() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
@@ -71,6 +98,21 @@ public class Driver {
                         xPos = 0;
                     }
                     repaint();
+                    if(yPos == y2Pos  && xPos==x2Pos)
+                    {
+                    	yPos = 0;
+                    	xPos = 0;
+                    }
+                    if(xPos < x2Pos)
+                    {
+                        int int_random = rand.nextInt(10 + 5) - 5;
+                        x2Pos -= int_random;
+                    }
+                    else
+                    {
+                        int int_random = rand.nextInt(10 + 5) - 5;
+                        x2Pos += int_random;
+                    }
                 }
             };
             Action rightAction = new AbstractAction() {
@@ -81,6 +123,22 @@ public class Driver {
                         xPos = getWidth() - 10;
                     }
                     repaint();
+                    if(yPos == y2Pos  && xPos==x2Pos)
+                    {
+                    	yPos = 0;
+                    	xPos = 0;
+                    }
+                    if(xPos > x2Pos)
+                    {
+                        int int_random = rand.nextInt(10 + 5) - 5;
+                        x2Pos += int_random;
+                    }
+                    else
+                    {
+                        int int_random = rand.nextInt(10 + 5) - 5;
+                        x2Pos -= int_random;
+                    }
+
                 }
             };
             Action downAction = new AbstractAction() {
@@ -91,6 +149,23 @@ public class Driver {
                         yPos = getWidth() - 10;
                     }
                     repaint();
+                    if(yPos == y2Pos  && xPos==x2Pos)
+                    {
+                    	yPos = 0;
+                    	xPos = 0;
+                    }
+                    if(yPos > y2Pos)
+                    {
+                    	
+                        int int_random = rand.nextInt(10 + 5) - 5;
+                        y2Pos += int_random;
+                    }
+                    else
+                    {
+                        int int_random = rand.nextInt(10 + 5) - 5;
+                        y2Pos -= int_random;	
+                    }
+
                 }
             };
             Action upAction = new AbstractAction() {
@@ -101,13 +176,41 @@ public class Driver {
                         yPos = 0;
                     }
                     repaint();
+                    if(yPos == y2Pos  && xPos==x2Pos)
+                    {
+                    	yPos = 0;
+                    	xPos = 0;
+                    }
+                    //generate random values from 0-24
+                    //random.nextInt(30 + 10) - 10;
+                    if(yPos< y2Pos)
+                    {
+                        int int_random = rand.nextInt(10 + 5) - 5;
+                        y2Pos -= int_random;                    	
+                    }
+                    else
+                    {
+                        int int_random = rand.nextInt(10 + 5) - 5;
+                        y2Pos += int_random; 
+                    }
+
                 }
+ 
             };
 
             bindKeyStroke(WHEN_IN_FOCUSED_WINDOW, "move.left", KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), leftAction);
             bindKeyStroke(WHEN_IN_FOCUSED_WINDOW, "move.right", KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), rightAction);
             bindKeyStroke(WHEN_IN_FOCUSED_WINDOW, "move.up", KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), upAction);
             bindKeyStroke(WHEN_IN_FOCUSED_WINDOW, "move.down", KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), downAction);
+            //////////////////////////////////////////////////////////////
+
+            
+            /*
+            bindKeyStroke2(WHEN_IN_FOCUSED_WINDOW, "move.left2", KeyStroke.getKeyStroke(KeyEvent.VK_A, 0), left2Action);
+            bindKeyStroke2(WHEN_IN_FOCUSED_WINDOW, "move.right2", KeyStroke.getKeyStroke(KeyEvent.VK_D, 0), right2Action);
+            bindKeyStroke2(WHEN_IN_FOCUSED_WINDOW, "move.up2", KeyStroke.getKeyStroke(KeyEvent.VK_W, 0), up2Action);
+            bindKeyStroke2(WHEN_IN_FOCUSED_WINDOW, "move.down2", KeyStroke.getKeyStroke(KeyEvent.VK_S, 0), down2Action);
+            */
         }
 
         protected void bindKeyStroke(int condition, String name, KeyStroke keyStroke, Action action) {
@@ -117,18 +220,29 @@ public class Driver {
             im.put(keyStroke, name);
             am.put(name, action);
         }
+        protected void bindKeyStroke2(int condition, String name, KeyStroke keyStroke, Action action) {
+            InputMap im = getInputMap(condition);
+            ActionMap am = getActionMap();
+
+            im.put(keyStroke, name);
+            am.put(name, action);
+        }
 
         @Override
         public Dimension getPreferredSize() {
-            return new Dimension(400, 400);
+            return new Dimension(200, 200);
         }
 
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             Graphics2D g2d = (Graphics2D) g.create();
-            
-            g2d.drawRect(xPos, yPos, 16, 16);
+            Graphics2D g3d = (Graphics2D) g.create();
+            g3d.drawRect(x2Pos, y2Pos, 10, 10);
+            Graphics2D g4d = (Graphics2D) g.create();
+            g3d.drawRect(y2Pos, x2Pos, 10, 10);
+            g2d.drawRect(xPos, yPos, 10, 10);
             g2d.dispose();
+            g3d.dispose();
         }
 
     }
